a6f8b6410c872e5050fdde630def9ad4b2994dbd
Amir Zaidi <azaidi@live.nl>
2018-04-11 20:44:08 +0200

    Rewrite Google Now Feed code based on Pixel Launcher P-4623511
    Change-Id: I170437896955a1e8bfa68db618fef593b1448388

--- src/com/google/android/apps/nexuslauncher/NexusLauncherActivity.java
+++ src/com/google/android/apps/nexuslauncher/NexusLauncherActivity.java
@@ -9,7 +9,7 @@ import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.util.ComponentKeyMapper;
-import com.google.android.libraries.launcherclient.GoogleNow;
+import com.google.android.libraries.gsa.launcherclient.LauncherClient;
 
 import java.util.List;
 
@@ -22,7 +22,7 @@ public class NexusLauncherActivity extends Launcher {
 
     protected void overrideTheme(boolean isDark, boolean supportsDarkText) {
         ContentResolver resolver = this.getContentResolver();
-        int flags = Utilities.getDevicePrefs(this).getInt("pref_persistent_flags", 0);
+        int flags = Utilities.getDevicePrefs(this).getInt(NexusLauncherOverlay.PREF_PERSIST_FLAGS, 0);
         int orientFlag = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE ? 16 : 8;
         boolean useGoogleInOrientation = (orientFlag & flags) != 0;
         int userThemeSetting = Settings.Secure.getIntForUser(resolver, Settings.Secure.DEVICE_THEME, 0, mCurrentUserId);
@@ -40,10 +40,10 @@ public class NexusLauncherActivity extends Launcher {
     }
 
     public List<ComponentKeyMapper<AppInfo>> getPredictedApps() {
-        return mLauncher.fA.getPredictedApps();
+        return mLauncher.mCallbacks.getPredictedApps();
     }
 
-    public GoogleNow getGoogleNow() {
-        return mLauncher.fy;
+    public LauncherClient getGoogleNow() {
+        return mLauncher.mClient;
     }
 }
